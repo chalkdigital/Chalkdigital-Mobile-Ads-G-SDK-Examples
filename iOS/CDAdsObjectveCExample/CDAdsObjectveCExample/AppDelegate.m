cc89538acd50b064e24dd3ed69f8830b2d658254
Arun Gupta <arun@chalkdigital.com>
2016-12-08 19:47:00 +0530

    updated sdk

--- iOS/CDAdsObjectveCExample/CDAdsObjectveCExample/AppDelegate.m
+++ iOS/CDAdsObjectveCExample/CDAdsObjectveCExample/AppDelegate.m
@@ -22,7 +22,7 @@
     params.secret = @"Your_CDAds_Secret";               //Provided by Chalkdigital;
     params.publisherId = @"chalkboard";                 //Provided by Chalkdigital;
     params.enableTracking = YES;                        //Set NO disable device information tracking
-    params.askForTrackingPermission = YES;              //Set NO to disable device information tracking
+    params.askForTrackingPermission = YES;              //Set NO to disable tracking permission dialog
     _cdAds = [CDAds initialiseWithParams:params];
     return YES;
 }
